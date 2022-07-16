5a61362fec72cb6e9cbd436273897cb5d45f4f6d
Furanimus <babitskyd@gmail.com>
2022-07-16 16:48:57 +0300

    Projectiles!

--- Tower Dicefense/Assets/Scripts/Enemy.cs
+++ Tower Dicefense/Assets/Scripts/Enemy.cs
@@ -9,11 +9,16 @@ public class Enemy : MonoBehaviour
 	public Transform enemy;
 	private Transform targetWaypoint;
 	private int waypointIndex = 0;
+	public Vector3 facingDirection;
+	public Quaternion lookRotation;
 
     // Start is called before the first frame update
     void Start()
     {
 		targetWaypoint = Waypoints.waypoints[0];
+		facingDirection = (targetWaypoint.position - transform.position).normalized;
+		lookRotation = Quaternion.LookRotation(facingDirection);
+		transform.Rotate(0f, 0f, 90f); //  = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
     }
 
     // Update is called once per frame
@@ -22,7 +27,11 @@ public class Enemy : MonoBehaviour
 		Vector3 direction = targetWaypoint.position - transform.position;
 		transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
 
-		if(Vector3.Distance(transform.position, targetWaypoint.position) < 0.2f)
+		facingDirection = (targetWaypoint.position - transform.position).normalized;
+		lookRotation = Quaternion.LookRotation(facingDirection);
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+
+		if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.2f)
 		{
 			nextWaypointOrDestroy();
 		}
