44475b73ee3d5b5e9e0093cf24ed08b90370da5a
Furanimus <babitskyd@gmail.com>
2022-07-16 03:01:42 +0300

    Added enemy spawning intervals
    Added enemy spawning intervals

---
+++ Tower Dicefense/Assets/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+	// public Transform wavedice;
+	public Transform enemy;
+	public Transform spawnPoint;
+	public Text waveCountdownText;
+
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
+	private int waveIndex = 0;
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (countdown <= 0f)
+		{
+			StartCoroutine(SpawnWave());
+			countdown = timeBetweenWaves;
+		}
+		countdown -= Time.deltaTime;
+		waveCountdownText.text = Mathf.Floor(countdown).ToString();
+	}
+	private IEnumerator SpawnWave()
+	{
+		waveIndex++;
+
+		for (int i = 0; i < waveIndex; i++)
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+	private void SpawnEnemy()
+	{
+		Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+	}
+}
